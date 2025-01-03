0b8b5a0863d06cee4260ae40689bd0a7647153aa
lucius <aebyl@bzz.ch>
2025-01-03 16:43:16 +0100

    saved results in a list

--- backend/openaiSearch.py
+++ backend/openaiSearch.py
@@ -2,15 +2,18 @@ import openai
 from googlesearch import search
 
 query = "Nestle BBC"
-for result in search(query, num_results=5):
+results = [result for result in search(query, num_results=5)]  # Save results in a list
+
+# Print the search results
+for result in results:
     print(result)
 
-openai.api_key = "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"
+openai.api_key = "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"
 chat_completion = openai.chat.completions.create(
     messages=[
         {
             "role": "user",
-            "content":f"is there any bad news mentioned in the following links?{result}",
+            "content":f"is there any bad news mentioned in the following links?{results}",
             "max_tokens":60
 
         }
